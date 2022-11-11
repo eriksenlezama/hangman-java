6a890122c66d687ba9ac8ee2c9b20a35535b7fb4
Eriksen Lezama <eriksen.lezama@gmail.com>
2022-11-10 21:11:20 -0600

    add printHangman method

--- Hangman.java
+++ Hangman.java
@@ -33,6 +33,7 @@ public class Hangman {
       }
 
       printFooter(livesLeft, lettersUsed);
+      printHangman(livesLeft);
 
       if (hasWon(revealed)) {
         break;
@@ -66,5 +67,34 @@ public class Hangman {
     } else {
       System.out.println("Nice! You guessed the word '" + answer + "'.");
     }
+  }
+
+  public static void printHangman(int livesLeft) {
+    String seven = " ___ \n|    \n|    \n|      \n|\n|";
+    String six = " ___ \n|   |\n|    \n|      \n|\n|";
+    String five = " ___ \n|   |\n|   O\n|      \n|\n|";
+    String four = " ___ \n|   |\n|   O\n|  /   \n|\n|";
+    String three = " ___ \n|   |\n|   O\n|  /|  \n|\n|";
+    String two = " ___ \n|   |\n|   O\n|  /|\\\n|\n|";
+    String one = " ___ \n|   |\n|   O\n|  /|\\\n|  /\n|";
+    String cero = " ___ \n|   |\n|   O\n|  /|\\\n|  / \\\n|";
+    if (livesLeft == 7) {
+      System.out.println(seven);
+    } else if (livesLeft == 6) {
+      System.out.println(six);
+    } else if (livesLeft == 5) {
+      System.out.println(five);
+    } else if (livesLeft == 4) {
+      System.out.println(four);
+    } else if (livesLeft == 3) {
+      System.out.println(three);
+    } else if (livesLeft == 2) {
+      System.out.println(two);
+    } else if (livesLeft == 1) {
+      System.out.println(one);
+    } else {
+      System.out.println(cero);
+    }
+    System.out.println();
   }
 }
